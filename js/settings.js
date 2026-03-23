e4830aa39e9087941769d537bcc5a2af3aeaa9fe
SalahXIX <sa2308761@qu.edu.qa>
2026-03-23 20:03:06 +0300

    apply logout and implement togglr button

--- js/settings.js
+++ js/settings.js
@@ -1,8 +1,30 @@
-import { requireAuth } from "./global/auth.js";
+import { requireAuth, logout } from "./global/auth.js";
 import { injectShell } from "./global/shell.js";
-import { applyTheme, getInitialTheme } from "./global/theme.js";
+import { applyTheme, getInitialTheme, setupThemeToggle } from "./global/theme.js";
 
 applyTheme(getInitialTheme());
 
 await requireAuth();
 await injectShell();
+
+const logoutBtn = document.getElementById("logout-btn");
+
+const ToggleButton = document.getElementById("settings-theme-toggle");
+
+if (logoutBtn) {
+  logoutBtn.addEventListener("click", () => { logout(); });
+}
+
+
+const TextLabel = document.getElementById("theme-toggle-label");
+
+if (ToggleButton && TextLabel) {
+  setupThemeToggle({ button: ToggleButton, onThemeChanged: (newTheme) => {
+    if (newTheme === "dark") {
+      TextLabel.textContent = "Dark mode";
+    } else{
+        TextLabel.textContent = "Light mode";
+    }
+  } });
+}
+
